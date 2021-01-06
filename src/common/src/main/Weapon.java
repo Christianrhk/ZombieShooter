56e351d6b17d5c049d12237d87d77dbc0b7d5295
Christianrhk <crhk1998@gmail.com>
2021-01-06 15:40:18 +0100

    Fix

--- src/common/src/main/Weapon.java
+++ src/common/src/main/Weapon.java
@@ -2,4 +2,17 @@ package common.src.main;
 
 public class Weapon {
 
+	private int DAMAGE, ATTACK_SPEED;
+	private double COST;
+	private String NAME;
+	
+	public Weapon(String name, int damage, int attackspeed, double cost) {
+		this.DAMAGE = damage;
+		this.NAME = name;
+		this.ATTACK_SPEED = attackspeed;
+		this.COST = cost;
+	}
+	
+	
+	
 }
