771d3ff7d54d8de9094592756694290f57ec7904
Christianrhk <crhk1998@gmail.com>
2021-01-06 15:32:17 +0100

    Test

--- src/common/src/main/Entity.java
+++ src/common/src/main/Entity.java
@@ -5,15 +5,20 @@ import java.awt.Point;
 public class Entity {
 	
 	String NAME;
-	int HEALTH_POINTS, ARMOR, DAMAGE, ATTACK_SPEED, MOVEMENT_SPEED;
+	int HEALTH_POINTS, ARMOR, DAMAGE, MOVEMENT_SPEED;
+	double ATTACK_SPEED, MONEY;
 	Point POSITION;
-	double MONEY;
 
 	// Direction.
 	
 	public Entity() {
-		// TODO Auto-generated constructor stub
-		/// testssssss
+		HEALTH_POINTS = 100;
+		ARMOR = 50;
+		DAMAGE = 10;
+		ATTACK_SPEED = 1.2;
+		MOVEMENT_SPEED = 10;
+		MONEY = 0;
+		POSITION = new Point(0,0);
 	}
 	
 }
