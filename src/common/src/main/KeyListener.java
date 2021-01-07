de4853063ee58a67f24397a61c4665ec904c5eb6
Christianrhk <crhk1998@gmail.com>
2021-01-07 08:25:10 +0100

    Added key listener class

---
+++ src/common/src/main/KeyListener.java
@@ -0,0 +1,35 @@
+package common.src.main;
+
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyListener extends KeyAdapter {
+	public void KeyPressed(KeyEvent e) {
+		int keyCode = e.getKeyCode();
+				
+		// Switch on pressed keys
+		switch(keyCode) {
+			case KeyEvent.VK_W:
+			case KeyEvent.VK_UP:
+				// Movement upwards
+				break;
+			case KeyEvent.VK_S:
+			case KeyEvent.VK_DOWN:
+				// Movement downwards
+				break;
+			case KeyEvent.VK_A:
+			case KeyEvent.VK_LEFT:
+				// Movement left
+				break;
+			case KeyEvent.VK_D:
+			case KeyEvent.VK_RIGHT:
+				// Movement right
+				break;
+		}
+	}
+	
+	public void KeyReleased() {
+		
+	}
+}
