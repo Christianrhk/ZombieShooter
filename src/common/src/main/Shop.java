dc97fdc009b3e5c6b468fa865e8a7bb6b0dbdcdf
DikalChrix <43571440+DikalChrix@users.noreply.github.com>
2021-01-07 09:18:15 +0100

    Tilføjet et shop class

---
+++ src/common/src/main/Shop.java
@@ -0,0 +1,45 @@
+package common.src.main;
+
+public class Shop {
+
+	public static void shopLogic(int ID, int money) {
+		
+		
+	//Varer i lageret
+	
+			
+	//Købslogik	
+		
+		
+	}
+	
+}
+
+
+class moneyUpdating implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
+
+class accessShop implements Runnable {
+	private int currentMoney;
+	
+	public accessShop (int currentMoney) {
+		this.currentMoney = currentMoney;
+	}	
+	
+	@Override
+	public void run() {
+		
+		
+		
+	}
+
+
+}
