1acb604d13178c125e6b32b7aad2968db285f24b
Christianrhk <crhk1998@gmail.com>
2021-01-08 10:27:58 +0100

    FIX

--- src/common/src/main/ContentsInFrame.java
+++ src/common/src/main/ContentsInFrame.java
@@ -81,7 +81,6 @@ public class ContentsInFrame extends JPanel implements KeyListener, ActionListen
         //Setting background image
         BufferedImage bg;
 		try {
-			System.out.println("Added background image");
 			bg = ImageIO.read(new File("src/images/zombiebanen.png"));
 	        g2d.drawImage(bg,0,0,this);
 		} catch (IOException e) {
