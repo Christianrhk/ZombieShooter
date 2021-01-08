1747d64fbc93f916a9d8f52a77ba51eca9a79424
koldbrandt <mailtilmb@gmail.com>
2021-01-08 12:14:25 +0100

    shop GUI cleanup

--- src/common/src/main/ContentShop.java
+++ src/common/src/main/ContentShop.java
@@ -6,12 +6,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class ContentShop extends JPanel implements KeyListener {
+public class ContentShop extends JPanel {
 
     private  boolean bHasBeenPressed;
     public ContentShop(){
         super.setDoubleBuffered(true);
-        addKeyListener(this);
         setFocusable(true);
         requestFocusInWindow();
         setFocusTraversalKeysEnabled(false);
@@ -31,32 +30,6 @@ public class ContentShop extends JPanel implements KeyListener {
         super.setLayout(new GridLayout(10,10));
         System.out.println("Has set Grid");
 
-
-
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (keyCode == KeyEvent.VK_B && bHasBeenPressed == false){
-            System.out.println("B has been pressed");
-            super.setVisible(true);
-            bHasBeenPressed = true;
-        }else if (keyCode == KeyEvent.VK_B){
-            System.out.println("B has been pressed 2 times and shop is closing");
-            super.setVisible(false);
-            bHasBeenPressed = false;
-        }
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
 }
